fde7ab6534c749ca51390f1f8236bd634d0df6ab
ferizoozoo <farhadzohoorz@gmail.com>
2021-09-18 09:32:50 +0430

    Add CORS & change react app port

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,13 @@ namespace feed
             services.AddDbContext<FeedDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
 
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));    
+
             services.AddControllersWithViews();
             services.AddTransient<IUserService, UserService>();
 
@@ -50,6 +57,8 @@ namespace feed
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("MyPolicy");
 
             app.UseMiddleware<JwtMiddleware>();
 
