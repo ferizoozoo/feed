c7dfed17a7d230f8274726ff9933af0fb4faa3a1
Farhad <farhadzohoorz@gmail.com>
2022-02-16 16:20:35 +0330

    Add migration files of Notification entity

---
+++ Migrations/20220216124946_AddNotificationEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace feed.Migrations
+{
+    public partial class AddNotificationEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
