94cc056ce7dee5cbeb355f315b4d090af47fa1d2
Farhad <farhadzohoorz@gmail.com>
2022-03-07 16:53:41 +0330

    Remove unnecessary repository method in NotificationRepository

--- Infrastructure/Repositories/Implements/NotificationRepository.cs
+++ Infrastructure/Repositories/Implements/NotificationRepository.cs
@@ -10,17 +10,9 @@ namespace feed.Infrastructure.Repositories.Implements
 {
     public class NotificationRepository : Repository<Notification>, INotificationRepository
     {
-        private readonly FeedDbContext _context;
         public NotificationRepository(FeedDbContext context) : base(context)
         {
-            _context = context;
-        }
 
-        public async Task<List<Notification>> GetNotificationsByUserId(int userId, int? pageNumber, int? pageSize)
-        {
-            return await _context.Set<Notification>().Where(x => x.ReceiverId == userId)
-                            .Skip(((int)pageNumber - 1) * (int)pageSize)
-                            .Take((int)pageSize).ToListAsync();
         }
     }
 }
--- Infrastructure/Repositories/Interfaces/INotificationRepository.cs
+++ Infrastructure/Repositories/Interfaces/INotificationRepository.cs
@@ -6,6 +6,6 @@ namespace feed.Infrastructure.Repositories.Interfaces
 {
     public interface INotificationRepository : IRepository<Notification>
     {
-        Task<List<Notification>> GetNotificationsByUserId(int userId, int? pageNumber, int? pageSize);
+        
     }
 }
